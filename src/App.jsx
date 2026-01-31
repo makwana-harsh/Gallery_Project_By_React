7c4a3b7a993b732d17f356a84f9dcf6a7121b400
Harsh <harshmakwana2706@gmail.com>
2026-01-31 23:26:30 +0530

    Tailwind css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div c>
+
+      </div>
     </>
   )
 }
